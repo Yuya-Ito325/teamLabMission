01c99be324f716b3f991ec511ac9386a4001fd21
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2022-03-30 11:33:49 +0900

    addFile

---
+++ teamLabMissionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teamLabMissionApp
+//
+//  Created by 伊藤祐哉 on 2022/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
